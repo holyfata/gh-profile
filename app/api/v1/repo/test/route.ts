eaf2a95d9056b4a525a9ee573f45389c70f8a07f
holyfata <holyfata@qq.com>
2025-06-01 23:02:12 +0800

    :art: clean the project

--- app/api/v1/repo/test/route.ts
+++
@@ -1,3 +0,0 @@
-export async function GET() {
-  return Response.json({ message: "Api Repo Test" });
-}
--- app/api/v1/user/name/route.ts
+++
@@ -1,7 +0,0 @@
-import { apiHandler } from "@/lib/url";
-import type { NextRequest } from "next/server";
-
-export async function GET(request: NextRequest) {
-  const url = request.nextUrl;
-  return await apiHandler(url);
-}
